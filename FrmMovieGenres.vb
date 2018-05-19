29dd769bf30cccd9c379e0f060277acba35fdfcf
Edward Manning <edward.manning@gmail.com>
2018-05-20 00:07:59 +0100

    #7: Tidyup

--- FrmMovieGenres.vb
+++ FrmMovieGenres.vb
@@ -1,6 +1,8 @@
 ﻿Public Class FrmMovieGenres
     Public Event MovieGenresAdded(ByVal genres As ListView.SelectedListViewItemCollection)
 
+    '======== EVENTS ============================================================
+
     Private Sub FrmMovieGenres_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
         lstGenres.Items.Clear()
         lstGenres.Columns.Add("Genre", 200, HorizontalAlignment.Left)
@@ -13,7 +15,7 @@
         selectArray(1) = 0
 
         'Shoot it over to the ReadRecord sub, 
-        Dim returnArray() As String = DbReadRecord(VideoDatabaseLocation, "SELECT * FROM genre", selectArray)
+        Dim returnArray() As String = DbReadRecord("SELECT * FROM genre", selectArray)
 
         'Now, read the output of the array.
         'Loop through each of the Array items.
@@ -28,7 +30,6 @@
 
             'Add the item to the TV genre list.
             lstGenres.Items.Add(itm)
-
         Next
 
         lstGenres.ListViewItemSorter = New clsListviewSorter(0, SortOrder.Ascending)
@@ -43,48 +44,47 @@
 
     Private Sub BtnNew_Click(ByVal sender As Object, ByVal e As EventArgs) Handles BtnNew.Click
         Dim newGenre = InputBox("New Genre Name", "New Genre Name")
+        If newGenre.Trim() = "" Then Return
 
-        If newGenre <> "" Then
-            Dim alreadyUsed As Boolean = False
-
-            For x = 0 To lstGenres.Items.Count - 1
-                If StrComp(lstGenres.Items(x).Text, newGenre, CompareMethod.Text) = 0 Then
-                    alreadyUsed = True
-                End If
-            Next
-
-            If alreadyUsed = False Then
-                DbExecute($"INSERT INTO genre (name) VALUES ('{newGenre}')")
-            Else
-                MsgBox($"You already have a genre labeled : {newGenre}")
+        Dim alreadyUsed As Boolean = False
+        For x = 0 To lstGenres.Items.Count - 1
+            If StrComp(lstGenres.Items(x).Text, newGenre, CompareMethod.Text) = 0 Then
+                alreadyUsed = True
             End If
-        End If
+        Next
 
+        If alreadyUsed = False Then
+            DbExecute($"INSERT INTO genre (name) VALUES ('{newGenre}')")
+        End If
+    
         FrmMovieGenres_Load(Nothing, Nothing)
     End Sub
 
     Private Sub BtnDelete_Click(ByVal sender As Object, ByVal e As EventArgs) Handles BtnDelete.Click
-        If lstGenres.SelectedItems.Count > 0 Then
-            DeleteMovieGenre()
-            RemoveGenreFromMovie()
+        If lstGenres.SelectedItems.Count <= 0 Then Return
+
+        DeleteMovieGenre()
+        RemoveGenreFromMovie()
             
-            FrmMovieGenres_Load(Nothing, Nothing)
-        End If
+        FrmMovieGenres_Load(Nothing, Nothing)
     End Sub
 
+    '======== SUBS ============================================================
+
     Private Sub DeleteMovieGenre() 
-        'These are the columns we need back.  Just the ID
+        'These are the columns we need back. Just the ID
         Dim selectArray(0)
         selectArray(0) = 0
 
         'Grab the GenreID and store it in GenreID
         Dim sqlStatement = $"SELECT * FROM genre WHERE name = '{lstGenres.SelectedItems(0).Text}'"
-        Dim returnArray() As String = DbReadRecord(VideoDatabaseLocation, sqlStatement, selectArray)
+        Dim returnArray() As String = DbReadRecord(sqlStatement, selectArray)
         Dim genreId = returnArray(0)
 
-        'Remove it from the Genre_link table for movies only
-        DbExecute($"DELETE FROM genre_link WHERE genre_id = '{genreId}' AND media_type = 'movie'")
+        'Remove it from the Genre_link table for tv shows and movies
+        DbExecute($"DELETE FROM genre_link WHERE genre_id = '{genreId}'")
 
+        'TODO - Merge Movie & TvShow Genres as there is only 1 set for both
         'Remove it from the Genres table
         DbExecute($"DELETE FROM genre WHERE genre_id = '{genreId}'")
     End Sub
@@ -96,7 +96,7 @@
         selectArray(1) = 9
 
         Dim statement = $"SELECT * FROM movie WHERE c14 LIKE '%{lstGenres.SelectedItems(0).Text}%'"
-        Dim returnArray() As String = DbReadRecord(VideoDatabaseLocation, statement, selectArray)
+        Dim returnArray() As String = DbReadRecord(statement, selectArray)
 
         'Make sure there's not a null return for the genre items.
         If returnArray Is Nothing Then
@@ -142,6 +142,7 @@
             Next
         End If
 
+        'TODO - Enhancement: Make this an Event so this can become a status bar update rather than msgbox
         MsgBox($"The genre {lstGenres.SelectedItems(0).Text} has also been removed from all movies")
     End Sub
 
